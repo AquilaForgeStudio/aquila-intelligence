de11f64a392c0bf67b79a5eb738e9249075376c3
Kevin <fasara140708@gmail.com>
2026-08-17 11:09:27 +0900

    Feat : added all sidebar-buttons

--- ui/components/sidebar.py
+++ ui/components/sidebar.py
@@ -8,17 +8,92 @@ class Sidebar(ctk.CTkFrame):
 
         self.pack_propagate(False)
 
-        button = SidebarButton(
+        Dashboard = SidebarButton(
             self,
-            icon="assets/icons/Planit-Logo.png",
+            icon="assets/icons/layout-dashboard.png",
             text="Dashboard",
             command=self.test
         )
 
-        button.pack(
+        Dashboard.pack(
             fill="x",
             padx=8,
-            pady=8
+            pady=4
+        )
+        Chat = SidebarButton(
+            self,
+            icon="assets/icons/message-circle.png",
+            text="Chat",
+            command=self.test
+        )
+
+        Chat.pack(
+            fill="x",
+            padx=8,
+            pady=4
+        )
+        History = SidebarButton(
+            self,
+            icon="assets/icons/rotate-ccw-clock.png",
+            text="History",
+            command=self.test
+        )
+
+        History.pack(
+            fill="x",
+            padx=8,
+            pady=4
+        )
+        Schedule = SidebarButton(
+            self,
+            icon="assets/icons/calendar-days.png",
+            text="Schedule",
+            command=self.test
+        )
+
+        Schedule.pack(
+            fill="x",
+            padx=8,
+            pady=4
+        )
+
+        Memo = SidebarButton(
+            self,
+            icon="assets/icons/notebook-pen.png",
+            text="Memo",
+            command=self.test
+        )
+
+        Memo.pack(
+            fill="x",
+            padx=8,
+            pady=4
+        )
+
+        Settings = SidebarButton(
+            self,
+            icon="assets/icons/settings.png",
+            text="Settings",
+            command=self.test
+        )
+
+        Settings.pack(
+            fill="x",
+            padx=8,
+            pady=4
+        )
+
+        Logout = SidebarButton(
+            self,
+            icon="assets/icons/log-out.png",
+            text="Logout",
+            command=self.test
+        )
+        Logout.text_label.configure(text_color="red")
+        Logout.pack(
+            fill="x",
+            padx=8,
+            pady=4
         )
 
     def test(self):
